18e57446ada7370ef88909dbe82072a15cb8b1be
Matthew Wade <romellem.git@gmail.com>
2017-04-28 14:55:02 -0500

    Fix formatting

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ var range = function(length) {
 };
 
 var allSame = function(a) {
-  var temp;
-  for (var i = 0; i < a.length; i++) {
-    if (typeof temp === 'undefined') {
-      temp = a[i];
-    } else {
-      if (temp !== a[i]) {
-        return false;
-      }
-    }
-  }
+	var temp;
+	for (var i = 0; i < a.length; i++) {
+		if (typeof temp === 'undefined') {
+			temp = a[i];
+		} else {
+			if (temp !== a[i]) {
+				return false;
+			}
+		}
+	}
 
-  return true;
+	return true;
 };
 
 var getRandomInt = function(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+	return Math.floor(Math.random() * (max - min)) + min;
 };
 
 /**
@@ -32,23 +32,14 @@ function game(box) {
 		var ball1 = box.splice(getRandomInt(0, box.length), 1)[0];
 		var ball2 = box.splice(getRandomInt(0, box.length), 1)[0];
 
-		// console.log('Painting ' + ball2 + ' to ' + ball1 + ', leaving ' + box.join(', ') + ', ' + ball1 + ', ' + ball1);
-
 		// "paint over" ball 2
 		ball2 = ball1;
 
 		box.push(ball1);
 		box.push(ball2);
 
-
 		turns++;
 	}
-
-	// console.log('==================');
-	// console.log('DONE');
-	// console.log('==================\n');
-	// console.log(box);
-	// console.log('Turns: ' + turns);
 
 	return turns;
 }
